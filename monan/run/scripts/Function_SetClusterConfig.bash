3ef5419a4300ccd401a948d50888d5e37bc8d582
paulo kubota <pkubota@gmail.com>
2024-09-04 21:56:05 -0300

    Add files via upload

---
+++ monan/run/scripts/Function_SetClusterConfig.bash
@@ -0,0 +1,54 @@
+#!/bin/bash -x
+
+function Function_SetClusterConfig() {
+local  EXP_RES=$1
+local  TypeGrid=${2}        #TypeGrid=variable_resolution
+local  mensage=$3
+echo ${mensage}
+if [ ${TypeGrid} = 'variable_resolution' ]; then
+#
+# selected ncores to submission job
+#
+
+case "`echo ${EXP_RES} | awk '{print $1/1 }'`" in
+  835586)cores_model=512 ;nodes_model=4 ;cores=256 ;cores_stat=32   ;nodes=2 ;; 	   #'060_003km' ;; 
+  535554)cores_model=256 ;nodes_model=2 ;cores=32  ;cores_stat=32   ;nodes=1 ;; 	   # 060_015km
+  163842)cores_model=128 ;nodes_model=1 ;cores=32  ;cores_stat=32   ;nodes=1 ;; 	   # 060_015km
+esac
+#
+# Configuracoes
+#
+JobElapsedTime=01:00:00   # Tempo de duracao do Job
+MPITasks=${cores}         # Numero de processadores que serao utilizados no Job
+TasksPerNode=${cores}     # Numero de processadores utilizados por tarefas MPI
+ThreadsPerMPITask=1       # Number of cores hosting OpenMP threads
+
+else
+
+case "`echo ${EXP_RES} | awk '{print $1/1 }'`" in
+65536002)cores_model=514 ;nodes_model=4 ;cores=256 ;cores_stat=32  ;nodes=2 ;;
+ 2621442)cores_model=256 ;nodes_model=2 ;cores=128 ;cores_stat=32  ;nodes=1 ;; 
+ 1024002)cores_model=128 ;nodes_model=1 ;cores=32  ;cores_stat=32  ;nodes=1 ;;     
+  655362)cores_model=4   ;nodes_model=1 ;cores=4   ;cores_stat=1   ;nodes=1 ;; 
+  256002)cores_model=20  ;nodes_model=1 ;cores=20  ;cores_stat=32  ;nodes=1 ;; 
+  163842)cores_model=16  ;nodes_model=1 ;cores=16  ;cores_stat=32  ;nodes=1 ;; 
+   40962)cores_model=4   ;nodes_model=4 ;cores=4   ;cores_stat=1   ;nodes=1 ;; 
+   10242)cores_model=8   ;nodes_model=1 ;cores=8   ;cores_stat=32  ;nodes=1 ;; 
+    4002)cores_model=6   ;nodes_model=1 ;cores=6   ;cores_stat=32  ;nodes=1 ;; 
+    2562)cores_model=2   ;nodes_model=1 ;cores=2   ;cores_stat=32  ;nodes=1 ;; 
+esac
+#
+# Configuracoes
+#
+JobElapsedTime=01:00:00   # Tempo de duracao do Job
+MPITasks=${cores}         # Numero de processadores que serao utilizados no Job
+TasksPerNode=${cores}     # Numero de processadores utilizados por tarefas MPI
+ThreadsPerMPITask=1       # Number of cores hosting OpenMP threads
+
+fi
+
+if [ "${MPITasks}" = "" ]; then
+echo "ERROR Function_SetClusterConfig ${EXP_RES}=>"${RES_KM}
+exit 5
+fi
+}
---
+++ monan/run/scripts/VarEnvironmental.bash
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+function VarEnvironmental() {
+    local  mensage=$1
+    echo   ${mensage}
+    export DIR_HOME=`cd ..;pwd`
+    export SUBMIT_HOME=`cd ..;pwd`
+    export DIRMONAN_PRE_SCR=${SUBMIT_HOME}   # will override scripts at MONAN
+    export DIRMONAN_MODEL_SCR=${DIR_HOME}    # will override scripts at MONAN
+    export DIRDADOS=/mnt/beegfs/monan/dados/MONAN_v0.1.0 
+    export path_mets=/home/paulo_kubota/lib/lib_${ESMF_COMPILER}/metis/bin
+
+    export NCARG_ROOT=/home/paulo.kubota/.conda/envs/ncl_stable    
+    export NCARG_BIN=${NCARG_ROOT}/bin
+
+    export NCAR_Tools=${SUBMIT_HOME}/pre/sources/${ESMF_COMPILER}/MPAS-Tools
+    export NCAR_MPAS=${NCAR_Tools}/MPAS-Limited-Area
+    export version_pos=convert_mpas_v0.1.0_egeon.${ESMF_COMPILER}940
+    export version_model=MONAN-Model_v1.0.0_egeon.${ESMF_COMPILER}940
+    export SLURM='NO'
+    export GREEN='\033[1;32m'  # Green
+    export RED='\033[1;31m'    # Red
+    export NC='\033[0m'        # No Color
+}
